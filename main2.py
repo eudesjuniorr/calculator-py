7b63ca2333d6a0209842a762cafb44a108d56824
Eudes Junior <eudes.junior@outlook.com.br>
2023-02-12 17:09:21 -0300

    added more functions

--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk # themed tk
+import math as mt
 
 current_operation = "basic" # variable that stores the current operation for the change_operations function
 
@@ -23,7 +24,7 @@ def button_add():
     global math
     math = "addition"
     f_num = first_number
-    number_input.set("")
+    number_input.set(str(f_num) + " + ")
 
 def button_subtract():
     first_number = float(number_input.get())
@@ -31,7 +32,7 @@ def button_subtract():
     global math
     math = "subtraction"
     f_num = first_number
-    number_input.set("")
+    number_input.set(str(f_num) + " - ")
 
 def button_multiply():
     first_number = float(number_input.get())
@@ -39,7 +40,7 @@ def button_multiply():
     global math
     math = "multiplication"
     f_num = first_number
-    number_input.set("")
+    number_input.set(str(f_num) + " * ")
 
 def button_divide():
     first_number = float(number_input.get())
@@ -47,7 +48,7 @@ def button_divide():
     global math
     math = "division"
     f_num = first_number
-    number_input.set("")
+    number_input.set(str(f_num) + " / ")
 
 def button_exponentiation():
     first_number = float(number_input.get())
@@ -55,7 +56,7 @@ def button_exponentiation():
     global math
     math = "exponentiation"
     f_num = first_number
-    number_input.set("")
+    number_input.set(str(f_num) + " ^ ")
 
 def button_radication():
     first_number = float(number_input.get())
@@ -63,33 +64,96 @@ def button_radication():
     global math
     math = "radication"
     f_num = first_number
-    number_input.set("")
+    number_input.set(str(f_num) + " √ ")
 
-def button_equal(): # function that calculates the result as soon as the equal button is pressed
-    second_number = float(number_input.get())
-    number_input.set("")
+def button_percent():
+    first_number = float(number_input.get())
+    global f_num
+    global math
+    math = "percent"
+    f_num = first_number
+    number_input.set(str(f_num) + " % ")
+
+def button_fraction():
+    first_number = float(number_input.get())
+    global f_num
+    global math
+    math = "fraction"
+    f_num = first_number
+    number_input.set(1 / f_num)
+
+def button_factorial():
+    first_number = float(number_input.get())
+    global f_num
+    global math
+    math = "factorial"
+    f_num = first_number
+    number_input.set(1)
+    if f_num < 0:
+        number_input.set("Não existe fatorial de número negativo")
+    else:
+        for i in range(1, int(f_num) + 1):
+            number_input.set(int(number_input.get()) * i)
+
+def button_log():
+    first_number = float(number_input.get())
+    global f_num
+    global math
+    math = "log"
+    f_num = first_number
+    if f_num < 0:
+        number_input.set("O resultado é um número complexo")
+    else:
+        number_input.set(mt.log(f_num, 10))
+
+def button_equal(): # calculates the result
+    second_number = float(number_input.get().split(" ")[2])
+    number_input.set(calculate(f_num, second_number, math))
+
+def calculate(f_num, second_number, math): # function that calculates the result
     if math == "addition":
-        number_input.set(f_num + second_number)
+        result = (f_num + second_number)
+        return result
     if math == "subtraction":
-        number_input.set(f_num - second_number)
+        result = (f_num - second_number)
+        return result
     if math == "multiplication":
-        number_input.set(f_num * second_number)
+        result = (f_num * second_number)
+        return result
     if math == "division":
-        number_input.set(f_num / second_number)
+        result = (f_num / second_number)
+        return result
     if math == "exponentiation":
-        number_input.set(f_num ** second_number)
+        result = (f_num ** second_number)
+        return result
     if math == "radication":
-        number_input.set(f_num ** (1/second_number))
+        result = (f_num ** (1/second_number))
+        return result
+    if math == "percent":
+        result = (second_number * f_num / 100)
+        return result
 
 def change_operations():
     global current_operation
     if current_operation == "basic":
-        btn_add.config(text="x^2")
-        btn_add.config(command=button_exponentiation)
-        current_operation = "exponentiation"
+        btn_add.config(text="%")
+        btn_add.config(command=button_percent)
+        btn_sub.config(text="1/x")
+        btn_sub.config(command=button_fraction)
+        btn_mult.config(text="n!")
+        btn_mult.config(command=button_factorial)
+        btn_div.config(text="log10")
+        btn_div.config(command=button_log)
+        current_operation = "button"
     else:
         btn_add.config(text="+")
         btn_add.config(command=button_add)
+        btn_sub.config(text="-")
+        btn_sub.config(command=button_subtract)
+        btn_mult.config(text="x")
+        btn_mult.config(command=button_multiply)
+        btn_div.config(text="/")
+        btn_div.config(command=button_divide)
         current_operation = "basic"
 
 root = tk.Tk()
@@ -120,11 +184,11 @@ btn_9.grid(row=1, column=2)
 btn_add = ttk.Button(root, text="+", width=11, command=button_add)
 btn_add.grid(row=1, column=3, sticky="w")
 
-btn_div = ttk.Button(root, text="-", width=11, command=button_subtract)
-btn_div.grid(row=1, column=3, sticky="e")
+btn_sub = ttk.Button(root, text="-", width=11, command=button_subtract)
+btn_sub.grid(row=1, column=3, sticky="e")
 
-btn_div = ttk.Button(root, text="+/-", width=5, command=number_negative_positive)
-btn_div.grid(row=1, column=4, rowspan=2, sticky="nsew")
+btn_nnp = ttk.Button(root, text="+/-", width=5, command=number_negative_positive)
+btn_nnp.grid(row=1, column=4, rowspan=2, sticky="nsew")
 
 # line 2
 btn_4 = ttk.Button(root, text="4", width=10, command=lambda: add_number(4))
@@ -136,8 +200,8 @@ btn_5.grid(row=2, column=1)
 btn_6 = ttk.Button(root, text="6", width=10, command=lambda: add_number(6))
 btn_6.grid(row=2, column=2)
 
-btn_sub = ttk.Button(root, text="*", width=11, command=button_multiply)
-btn_sub.grid(row=2, column=3, sticky="w")
+btn_mult = ttk.Button(root, text="*", width=11, command=button_multiply)
+btn_mult.grid(row=2, column=3, sticky="w")
 
 btn_div = ttk.Button(root, text="/", width=11, command=button_divide)
 btn_div.grid(row=2, column=3, sticky="e")
@@ -152,14 +216,14 @@ btn_2.grid(row=3, column=1)
 btn_3 = ttk.Button(root, text="3", width=10, command=lambda: add_number(3))
 btn_3.grid(row=3, column=2)
 
-btn_mul = ttk.Button(root, text="x^y", width=11, command=button_exponentiation)
-btn_mul.grid(row=3, column=3, sticky="w")
+btn_exp = ttk.Button(root, text="x^y", width=11, command=button_exponentiation)
+btn_exp.grid(row=3, column=3, sticky="w")
 
-btn_div = ttk.Button(root, text="√^y", width=11, command=button_radication)
-btn_div.grid(row=3, column=3, sticky="e")
+btn_rad = ttk.Button(root, text="√^y", width=11, command=button_radication)
+btn_rad.grid(row=3, column=3, sticky="e")
 
-btn_div = ttk.Button(root, text="mode", width=5, command=change_operations)
-btn_div.grid(row=3, column=4, rowspan=2, sticky="nsew")
+btn_co = ttk.Button(root, text="mode", width=5, command=change_operations)
+btn_co.grid(row=3, column=4, rowspan=2, sticky="nsew")
 
 # line 4
 btn_clear = ttk.Button(root, text="Clear", width=10, command=clear_display)
